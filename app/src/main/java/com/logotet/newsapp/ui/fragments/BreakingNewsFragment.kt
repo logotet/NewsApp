56c308775520058361b38f4084add606140a500b
logotet <logotetsp@gmail.com>
2021-11-23 12:48:36 +0200

    s

---
+++ app/src/main/java/com/logotet/newsapp/ui/fragments/BreakingNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.logotet.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.logotet.newsapp.R
+
+class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
+}
---
+++ app/src/main/java/com/logotet/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,7 @@
+package com.logotet.newsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.logotet.newsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+}
