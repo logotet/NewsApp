f73c46c32747cc613519ebded74a535563fcc481
logotet <logotetsp@gmail.com>
2021-11-23 13:33:39 +0200

    Retrofit JSON model

---
+++ app/src/main/java/com/logotet/newsapp/ui/models/NewsResponse.kt
@@ -0,0 +1,7 @@
+package com.logotet.newsapp.ui.models
+
+data class NewsResponse(
+    val articles: List<Article>,
+    val status: String,
+    val totalResults: Int
+)
---
+++ app/src/main/java/com/logotet/newsapp/ui/models/Source.kt
@@ -0,0 +1,6 @@
+package com.logotet.newsapp.ui.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
